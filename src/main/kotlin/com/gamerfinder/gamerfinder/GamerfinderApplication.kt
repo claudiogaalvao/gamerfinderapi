2c83cd83c5f31f24e6da99a288b915e29cf62334
Claudio Galvão <claudiogaalvao@gmail.com>
2025-01-14 22:33:54 -0300

    Initial commit

---
+++ src/main/kotlin/com/gamerfinder/gamerfinder/GamerfinderApplication.kt
@@ -0,0 +1,11 @@
+package com.gamerfinder.gamerfinder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GamerfinderApplication
+
+fun main(args: Array<String>) {
+	runApplication<GamerfinderApplication>(*args)
+}
---
+++ src/test/kotlin/com/gamerfinder/gamerfinder/GamerfinderApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gamerfinder.gamerfinder
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GamerfinderApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
